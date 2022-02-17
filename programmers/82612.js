57cd8ebd2352cb45ad71f5ba8a957e8f3c43659d
jinwoo <wwo1278@gmail.com>
2022-02-17 11:09:48 +0900

    부족한 금액 계산하기

---
+++ programmers/82612.js
@@ -0,0 +1,10 @@
+// 부족한 금액 계산하기
+
+function solution(price, money, count) {
+    for (let i = 1; i <= count; i++) {
+        money -= price * i;
+    }
+    return money < 0 ? Math.abs(money) : 0;
+}
+
+console.log(solution(3,20,4))
