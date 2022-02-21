8d14194c63a39391b4234f934bf672946fc6715e
jinwoo <wwo1278@gmail.com>
2022-02-22 00:31:16 +0900

    ??!

--- practice.js
+++ practice.js
@@ -267,30 +267,37 @@ const graph = {
     return visited; // 탐색한 순서대로 출력
   }
   
-  // 노드 연결 정보
-    const recursiveGraph = [
-        [],
-        [2, 3, 8],
-        [1, 7],
-        [1, 4, 5],
-        [3, 5],
-        [3, 4],
-        [7],
-        [2, 6, 8],
-        [1, 7]
-    ];
+function dfs(graph, v, visited) {
+  // 현재 노드 방문
+  visited[v] = true;
+  // 방문 노드 출력
+  console.log(v);
 
-  
-  const visited = new Array(recursiveGraph.length).fill(false);
-
-  function recursiveDFS( graph, v, visited){
-        visited[v] = true;
-        console.log(v);
-        graph[v].forEach(i=>{
-            if(!visited[i]) recursiveDFS(graph, i, visited);
-        })
-        return v;
+  // 인접 노드 탐색
+  graph[v].forEach(i => {
+    // 방문하지 않았다면
+    if (!visited[i]) {
+      // 재귀 호출
+      dfs(graph, i, visited);
     }
+  })
+}
 
-  console.log(DFS(graph, "A"));
-  console.log(BFS(graph, "A"));
+// 노드 연결 정보
+const graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+];
+
+// 방문 정보
+const visited = new Array(graph.length).fill(false);
+
+// 호출
+dfs(graph, 1, visited);
