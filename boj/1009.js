5b94efa509e1803a1f7d023fd69d7477be328f24
jinwoo <wwo1278@gmail.com>
2022-03-19 16:38:25 +0900

    수정

--- boj/1009.js
+++ boj/1009.js
@@ -5,23 +5,20 @@ const input = require('fs').readFileSync(filePath).toString().trim().split(/\n/)
 const T = parseInt(input.shift());
 const testCase = input;
 const sol = (T,testCase) =>{
-    const result = [];
-    const pattern = [[10],
-                    [1],
-                    [2,4,6,8],
-                    [3,9,7,1],
-                    [4,6],
-                    [5],
-                    [6],
-                    [7,9,3,1],
-                    [8,4,2,6],
-                    [9,1]];
-    for(let i=0; i<T;i++){
-        let [a,b] = testCase[i].split(' ');
-        const index = b%pattern[a%10].length-1;
-        result.push(pattern[a%10][index === -1? pattern[a%10].length-1:index]);
-    }
-    console.log(result.join('\n'));
+    const result = testCase.map(v => {
+        const [a, b] = v.split(' ');  
+          
+        let pow = 1;
+        
+        for (let j = 0; j < b; j++) {
+          pow = (pow * a) % 10;
+        }
+          
+        return pow === 0 ? 10 : pow;
+      });  
+      
+    console.log(result.join("\n"));
+
 }
 
 sol(T,testCase);
