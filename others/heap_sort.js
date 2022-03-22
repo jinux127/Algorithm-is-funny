407ded772bf2cb189b3e96cb9fa7ff843f79cb67
jinwoo <wwo1278@gmail.com>
2022-03-22 10:40:46 +0900

    힙정렬

--- others/heap_sort.js
+++ others/heap_sort.js
@@ -1,40 +1,40 @@
-const str =('7 5 6 8 3 5 9 1 6');
-const arr = str.split(' ').map(Number);
+// const str =('7 5 6 8 3 5 9 1 6');
+// const arr = str.split(' ').map(Number);
 
-const swap = (arr, i, j) =>{
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+// const swap = (arr, i, j) =>{
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
 
-    return arr;
-}
+//     return arr;
+// }
 
-const heapify = (arr, lastIdx) =>{
-    let last = parseInt(lastIdx/2)-1;
-    while(last >= 0){
-        const left = last*2+1;
-        const right = last*2+2;
+// const heapify = (arr, lastIdx) =>{
+//     let idx = parseInt(lastIdx/2)-1;
+//     while(idx >= 0){
+//         const left = idx*2+1;
+//         const right = idx*2+2;
 
-        if(arr[left] >= arr[right] && arr[last] < arr[left]){
-            arr = swap(arr,last,left);
-        } else if(arr[left] < arr[right] && arr[last] < arr[right]){
-            arr = swap(arr,last,right);
-        }
-        last--;
-    }
-    return arr;
-}
-const heapSort = (arr) =>{
-    for(let i=arr.length-1; i>=0; i--){
-        arr = heapify(arr,i);
-        if(arr[0] > arr[i]){
-            arr = swap(arr,i, 0);
-        }
-    }
-    return arr;
-}
+//         if(arr[left] >= arr[right] && arr[idx] < arr[left]){
+//             arr = swap(arr,idx,left);
+//         } else if(arr[left] < arr[right] && arr[idx] < arr[right]){
+//             arr = swap(arr,idx,right);
+//         }
+//         idx--;
+//     }
+//     return arr;
+// }
+// const heapSort = (arr) =>{
+//     for(let i=arr.length-1; i>=0; i--){
+//         arr = heapify(arr,i);
+//         if(arr[0] > arr[i]){
+//             arr = swap(arr,i, 0);
+//         }
+//     }
+//     return arr;
+// }
 
-console.log(heapSort(arr));
+// console.log(heapSort(arr));
 
 // const heap_sort = (arr) =>{
 //     // 전체 트리 구조를 최대 힙 구조로 변환
@@ -82,3 +82,49 @@ console.log(heapSort(arr));
 // }
 
 // heap_sort(arr);
+
+let arrLen;
+
+function swap(arr, i, j){
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function heapify(arr, i){
+  let max = i;
+  let left = 2 * i + 1;
+  let right = 2 * i + 2;
+  
+  if(left < arrLen && arr[left] > arr[max]){
+    max = left;
+  }
+  
+  if(right < arrLen && arr[right] > arr[max]){
+    max = right;
+  }
+  
+  if(max != i){
+    swap(arr, i, max);
+    heapify(arr, max);
+  }
+}
+
+function heapSort(arr){
+  arrLen = arr.length;
+
+  for(let i = Math.floor(arrLen / 2); i >= 0; i--){
+    heapify(arr, i);
+  }
+  
+  for(let i = arrLen - 1; i > 0; i--){
+    swap(arr, 0, i);
+    arrLen--;
+    
+    heapify(arr, 0);
+  }
+  
+  return arr;
+}
+
+console.log(heapSort([13, 1, 3, 7, 9, 11, 5]));
