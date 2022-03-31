fa41eeef4dcd33add737c9f38db6b84975c236ca
jinwoo <wwo1278@gmail.com>
2022-03-31 20:18:22 +0900

    카드 구매하기 1,2 / 나의 인생은 수학과 함께

---
+++ boj/11052.js
@@ -0,0 +1,20 @@
+// 카드 구매하기
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require('fs').readFileSync(filePath).toString().trim().split(/\n/);
+const N = parseInt(input.shift());
+const numArr = input[0].split(' ').map(Number);
+
+const sol = (N, numArr) =>{
+    let dp = [0,...numArr];
+
+    for(let i=2; i<=N; i++){
+        for(let j=1; j<i; j++){
+            dp[i] = Math.max(dp[i], (dp[i-j]+dp[j]));
+        }
+    }
+    
+    console.log(dp[N]);
+}
+
+sol(N, numArr);
