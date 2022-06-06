4b527d4793bae8c6204eed97d71590aac905c6dc
jinwoo <wwo1278@gmail.com>
2022-06-06 15:53:16 +0900

    오픈채팅방

--- programmers/42888.js
+++ programmers/42888.js
@@ -1,6 +1,28 @@
 // 오픈채팅방
 
 function solution(record) {
-  var answer = [];
+  let answer = [];
+  const arr = [];
+  const id = {};
+  record.map((item) => {
+    answer.push(item.split(' '));
+  });
+  console.log(answer);
+  answer.map((item) => {
+    if (item[0] !== 'Change') {
+      arr.push([item[0], item[1]]);
+      if (item[0] !== 'Leave') id[item[1]] = item[2];
+    } else {
+      id[item[1]] = item[2];
+    }
+  });
+  console.log(arr);
+  console.log(id);
+  answer = arr.map((item) => {
+    return item[0] === 'Enter' ? `${id[item[1]]}님이 들어왔습니다.` : `${id[item[1]]}님이 나갔습니다.`;
+  });
+  console.log(answer);
   return answer;
 }
+
+solution(['Enter uid1234 Muzi', 'Enter uid4567 Prodo', 'Leave uid1234', 'Enter uid1234 Prodo', 'Change uid4567 Ryan']);
