63c98378059432b07e03eebd81f6fbdcf69dcba2
jinwoo <wwo1278@gmail.com>
2022-06-07 13:23:41 +0900

    메뉴리뉴얼~!

---
+++ programmers/메뉴-리뉴얼.js
@@ -0,0 +1,8 @@
+function solution(orders, course) {
+  var answer = [];
+  const menu = new Set(...orders.map((item) => item.split('')));
+
+  return answer;
+}
+
+solution(['ABCFG', 'AC', 'CDE', 'ACDE', 'BCFG', 'ACDEH'], [2, 3, 4]);
