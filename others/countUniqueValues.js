7fe976e9b1f35524483aa715f9678c0571205a71
jinwoo <wwo1278@gmail.com>
2022-06-21 00:22:02 +0900

    투포인터

---
+++ others/countUniqueValues.js
@@ -0,0 +1,17 @@
+function countUniqueValues(arr) {
+  // add whatever parameters you deem necessary - good luck!
+  if (arr.length === 0) return 0;
+
+  let idx = 0;
+  const result = [];
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[idx] !== arr[j]) {
+      idx += 1;
+      arr[idx] = arr[j];
+    }
+  }
+  return idx + 1;
+}
+
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 6, 6, 12, 12, 13]));
