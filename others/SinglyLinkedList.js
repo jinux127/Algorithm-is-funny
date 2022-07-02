26e91a161a5494444bc440a615036bb6497c1195
jinwoo <wwo1278@gmail.com>
2022-07-02 23:45:18 +0900

    노드

--- others/SinglyLinkedList.js
+++ others/SinglyLinkedList.js
@@ -23,4 +23,23 @@ class SinglyLinkedList {
     this.legnth += 1;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
