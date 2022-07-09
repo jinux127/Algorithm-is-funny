0c261f368c261e1146f0656561b1ce45faaf968b
jinwoo <wwo1278@gmail.com>
2022-07-09 22:50:01 +0900

    unshift()

--- others/SinglyLinkedList.js
+++ others/SinglyLinkedList.js
@@ -53,4 +53,16 @@ class SinglyLinkedList {
 
     return current;
   }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
 }
