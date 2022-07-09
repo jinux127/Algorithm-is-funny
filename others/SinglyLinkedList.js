369591cf6ed37985a1c67baa3c090537785b324a
jinwoo <wwo1278@gmail.com>
2022-07-09 23:55:04 +0900

    reverse????

--- others/SinglyLinkedList.js
+++ others/SinglyLinkedList.js
@@ -116,7 +116,19 @@ class SinglyLinkedList {
     return removed;
   }
 
-  reverse() {}
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+  }
 }
 
 const L = new SinglyLinkedList();
