1bb24ef74be22beed006092fb0c45536473ced9b
jinwoo <wwo1278@gmail.com>
2022-07-11 01:22:00 +0900

    unshift, get

--- data-structure/DoublyLinkedList.js
+++ data-structure/DoublyLinkedList.js
@@ -56,6 +56,40 @@ class DoublyLinkedList {
     this.length -= 1;
     return oldHead;
   }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let cnt = 0;
+    let value = null;
+    if (index < this.length / 2) {
+      value = this.head;
+      while (cnt === index) {
+        value = this.head.next;
+        cnt += 1;
+      }
+    } else {
+      value = this.tail;
+      cnt = this.length - 1;
+      while (cnt === index) {
+        value = this.head.prev;
+        cnt -= 1;
+      }
+    }
+    return value;
+  }
 }
 
 const DLL = new DoublyLinkedList();
@@ -69,4 +103,5 @@ DLL.push(16);
 console.log(DLL.pop());
 console.log(DLL.pop());
 console.log(DLL.pop());
-console.log(DLL);
+console.log(DLL.get(0));
+// console.log(DLL);
