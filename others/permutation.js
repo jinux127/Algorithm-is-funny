94c1305e98213029c0905d0b2466825bf26a84a4
jinwoo <wwo1278@gmail.com>
2022-08-05 21:58:14 +0900

    0805

--- others/permutation.js
+++ others/permutation.js
@@ -1,31 +1,30 @@
 // 순열
 
-const arr = [1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
 const MAX = arr.length;
 const visited = new Array(MAX).fill(false);
 const vector = [];
 const result = [];
 
-const dfs = (cnt) =>{
-    let str ="";
-    if(cnt === 2){
-        for(let i=0; i<vector.length; i++){
-            str += vector[i] + " ";
-        }
-        result.push(str.trim());
-        return;
+const dfs = (cnt) => {
+  let str = '';
+  if (cnt === 2) {
+    for (let i = 0; i < vector.length; i++) {
+      str += vector[i] + ' ';
     }
+    result.push(str.trim());
+    return;
+  }
 
-    for(let i=0; i<MAX; i++){
-        if(visited[i]) continue;
-        visited[i] = true;
-        vector.push(arr[i]);
-        dfs(cnt+1);
-        vector.pop();
-        visited[i] = false;
-    }
-
-}
+  for (let i = 0; i < MAX; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    vector.push(arr[i]);
+    dfs(cnt + 1);
+    vector.pop();
+    visited[i] = false;
+  }
+};
 
 dfs(0);
 console.log(result.join('\n'));
---
+++ programmers/소수-찾기.js
@@ -0,0 +1,39 @@
+function solution(numbers) {
+  const arr = numbers.split('');
+  const temp = [];
+  const visited = new Array(numbers.length).fill(false);
+  const result = [];
+  const permutation = (cnt, targetNumber) => {
+    let str = '';
+    if (cnt === targetNumber) {
+      for (let i = 0; i < temp.length; i++) {
+        str += temp[i];
+      }
+      result.push(str.trim());
+      return;
+    }
+
+    for (let i = 0; i < numbers.length; i++) {
+      if (visited[i]) continue;
+      visited[i] = true;
+      temp.push(arr[i]);
+      permutation(cnt + 1, targetNumber);
+      temp.pop();
+      visited[i] = false;
+    }
+  };
+  for (let i = 1; i <= numbers.length; i++) {
+    permutation(0, i);
+  }
+
+  return [...new Set(result.map(Number).filter(isPrime))].length;
+}
+
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return num >= 2;
+}
+
+console.log(solution('011'));
