55a53c42b2b4a80446d0563dc8846d59d592d452
jinwoo <wwo1278@gmail.com>
2022-12-22 11:50:28 +0900

    1222

--- programmers/콜라-문제.js
+++ programmers/콜라-문제.js
@@ -1,3 +1,13 @@
-function solution(a, b, n) {}
+function solution(a, b, n) {
+  let answer = 0;
+  while (n >= a) {
+    const returnCola = Math.floor(n / a) * b;
+    const otherCola = n % a;
+    console.log(returnCola, otherCola);
+    answer += returnCola;
+    n = returnCola + otherCola;
+  }
+  return answer;
+}
 
-console.log(solution(2, 1, 20));
+console.log(solution(3, 2, 20));
