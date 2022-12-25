4dd2f130796ba813ef449045a980feb7c16159d8
jinwoo <wwo1278@gmail.com>
2022-12-25 22:27:18 +0900

    1225

--- javascript/프로미스 실습.js
+++ javascript/프로미스 실습.js
@@ -20,29 +20,57 @@ const getData_reject = () => {
   });
 };
 
-getData().then((res) => console.log(res));
-getData_reject()
-  .then((res) => console.log(res))
-  .catch((e) => console.log(e));
+// getData().then((res) => console.log(res));
 
-const firstPromise = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res('firstPromise success');
-    }, 2000);
-  });
-};
-const secondPromise = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res('secondPromise success');
-    }, 2000);
-  });
-};
-const thirdPromise = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res('thirdPromise success');
-    }, 2000);
-  });
-};
+// getData_reject()
+//   .then((res) => console.log(res))
+//   .catch((e) => console.log(e));
+
+const firstPromise = new Promise((res, rej) => {
+  setTimeout(() => {
+    res('firstPromise success');
+  }, 2000);
+});
+
+const secondPromise = new Promise((res, rej) => {
+  setTimeout(() => {
+    res('secondPromise success');
+  }, 3000);
+});
+
+const thirdPromise = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej('거부');
+    // res('thirdPromise success');
+  }, 1000);
+});
+
+// Promise.all([firstPromise, secondPromise, thirdPromise]).then((val) => {
+//   console.log(val);
+// });
+/**
+ * all 에서 error를 처리하고 싶으면 catch로 사전작업해주면 됨
+ */
+Promise.all([firstPromise, secondPromise, thirdPromise.catch((err) => err)]).then((val) => {
+  console.log(val);
+});
+
+/**
+ * all과 달리 실패를 하더라도 then을 통해 실행된다. 
+ * 배열의 형태로
+ * [
+    { status: 'fulfilled', value: 'firstPromise success' },
+    { status: 'fulfilled', value: 'secondPromise success' },
+    { status: 'fulfilled', value: '거부' }
+  ]
+ */
+Promise.allSettled([firstPromise, secondPromise, thirdPromise.catch((err) => err)]).then((val) => {
+  console.log(val);
+});
+
+/**
+ * 실패든 성공이든 가장먼저 완료된 결과를 반환함.
+ */
+Promise.race([firstPromise, secondPromise, thirdPromise.catch((err) => err)]).then((val) => {
+  console.log(val);
+});
