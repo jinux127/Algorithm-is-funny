5756d34adc5f24d04ea185b323cc37f68d858fe0
jinwoo <wwo1278@gmail.com>
2023-05-04 17:40:06 +0900

    2020-b-1

--- C/2020-b-1.c
+++ C/2020-b-1.c
@@ -7,13 +7,11 @@ int main(){
   char *p;
   count = strlen(data[0]);
   p = (char *) data;
-  // printf("%s",&data[1][0]);
-  printf("%c",*(p+2));
+  printf("%s",&data[1][0]);
   printf("\n");
   for ( k = 0; k < count; k++)
   {
     printf("%c", *(p+count - (k+1)));
   }
   return 0;
-  
 }
